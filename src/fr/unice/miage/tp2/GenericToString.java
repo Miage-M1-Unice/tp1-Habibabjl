ecc44d96a29d1d8fcc961512b99eeb4e2015340e
BOUAJLA <bouajla.h@hotmail.fr>
2019-02-18 20:56:19 +0100

    TP2 : début exercice 2

---
+++ src/fr/unice/miage/tp2/GenericToString.java
@@ -0,0 +1,9 @@
+package fr.unice.miage.tp2;
+
+public class GenericToString {
+
+    @Override
+    public String toString() {
+        return "GenericToString{}";
+    }
+}
---
+++ src/fr/unice/miage/tp2/Main.java
@@ -0,0 +1,7 @@
+package fr.unice.miage.tp2;
+
+public class Main {
+    public static void main(String args[]) {
+
+    }
+}
